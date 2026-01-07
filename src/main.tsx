52ade9d9aa426c73e7251ad4e645760d5eb68800
YasminSBarata <yasminteo1@gmail.com>
2026-01-07 12:49:04 -0300

    :bug: fix: Corrige imports e caminho do CSS no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
+import "./styles/index.css";
 
 // criando o gerenciador de queries
 const queryClient = new QueryClient({
@@ -13,13 +13,13 @@ const queryClient = new QueryClient({
       retry: 1, // se der error, tenta mais uma vez antes de desistir
     },
   },
-})
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* vai disponibilizar para toda a aplicação. CONTEXT PROVIDER: faz o query client ficar disponivel em qualquer lugar do app, qualquer componente pode usar o useQuery */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>
-)
+  </StrictMode>,
+);
